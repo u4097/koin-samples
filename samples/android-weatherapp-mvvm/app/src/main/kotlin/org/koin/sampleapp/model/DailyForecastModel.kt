19844b5157d4ee2a3e0853cacfdb80e48dbc0f1d
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2019-03-09 14:45:36 +0300

    reformat code

--- samples/android-weatherapp-mvvm/app/src/main/kotlin/org/koin/sampleapp/model/DailyForecastModel.kt
+++ samples/android-weatherapp-mvvm/app/src/main/kotlin/org/koin/sampleapp/model/DailyForecastModel.kt
@@ -5,7 +5,10 @@ import java.util.*
 /**
  * Represents our weather forecast for one day
  */
-data class DailyForecastModel(val forecastString: String, val icon: String, val temperatureLow: String, val temperatureHigh: String) {
+data class DailyForecastModel(val forecastString: String,
+                              val icon: String,
+                              val temperatureLow: String,
+                              val temperatureHigh: String) {
 
     val id = UUID.randomUUID().toString()
 
